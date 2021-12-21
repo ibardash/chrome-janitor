735acab2ece304309e5adc4a84b6536bd1d85d07
iana.bardash <iana.bardash@ladbrokes.com.au>
2021-12-21 15:40:47 +1100

    feat: wip handle close tab

--- public/app/scripts/handleCloseTab.js
+++ public/app/scripts/handleCloseTab.js
@@ -1,24 +1,58 @@
-chrome.tabs.onCreated.addListener((tab) => {
-  chrome.storage.sync.get("tabsInfo", function (value) {
-    const updatedTabsInfo = {
-      ...value,
-      [tab.id]: {
-        createdAt: Date.now(),
-      },
+chrome.tabs.onActivated.addListener((tab) => {
+  console.log("onActivated", tab);
+
+  chrome.storage.sync.get("tabsInfo", function (tabsInfo) {
+    console.log("tabsInfo", tabsInfo);
+    // set previous active tab active state to false
+    const previousActiveTab = Object.keys(tabsInfo).filter(
+      (key) => tabsInfo[key].isCurrent === true
+    );
+
+    if (previousActiveTab) {
+      tabsInfo[previousActiveTab] = {
+        isCurrent: false,
+        lastActivated: Date.now(),
+      };
+    }
+
+    // update info for the current active tab
+    tabsInfo[tab.tabId] = {
+      isCurrent: true,
+      lastActivated: null,
     };
 
-    chrome.storage.sync.set({ tabsInfo: updatedTabsInfo }, function () {
-      chrome.storage.sync.get("tabsInfo", function (value) {
-        console.log("tabsInfo", value);
-      });
-    });
+    // save the updated tab info to storage
+    chrome.storage.sync.set({ tabsInfo });
   });
+});
 
-  chrome.tabs.query({}, function (tabs) {
-    tabs.forEach((tab) => {
-      // const customTabInfo = chrome.storage.sync.get(key, function (value) {
-      //   console.log("stored value for tab", value);
-      // });
+chrome.alarms.create("closeTab", {
+  delayInMinutes: 0,
+  periodInMinutes: 1,
+});
+
+const MAX_TIME = 10000;
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "closeTab") {
+    chrome.storage.sync.get("tabsInfo", function (tabsInfo) {
+      chrome.tabs.query({}, function (tabs) {
+        tabs.forEach((tab) => {
+          const lastActivated = tabsInfo[tab.id].lastActivated;
+          if (lastActivated !== null) {
+            if (Date.now - lastActivated > MAX_TIME) {
+              // remove the tab
+              chrome.tabs.remove(tab.id);
+
+              // remove the key for tabsInfo
+              delete tabsInfo[tab.id];
+            }
+          }
+        });
+      });
+
+      // save tabsInfo back to storage
+      chrome.storage.sync.set({ tabsInfo });
     });
-  });
+  }
 });
