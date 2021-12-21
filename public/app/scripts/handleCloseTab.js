9d3f90681c14df03fb3c401994564118b6aab53f
iana.bardash <iana.bardash@ladbrokes.com.au>
2021-12-21 16:27:57 +1100

    style: remove console logs

--- public/app/scripts/handleCloseTab.js
+++ public/app/scripts/handleCloseTab.js
@@ -20,8 +20,6 @@ chrome.tabs.onActivated.addListener(async (tab) => {
   };
 
   // save the updated tab info to storage
-  console.log("onActivated setting tabsInfo", tabsInfo);
-
   chrome.storage.sync.set({ tabsInfo });
 });
 
@@ -35,13 +33,9 @@ chrome.storage.sync.set({ tabsInfo: {} });
 const MAX_TIME = 1000;
 
 chrome.alarms.onAlarm.addListener(async (alarm) => {
-  console.log("alarm fired", alarm.name);
-
   if (alarm.name !== "closeTab") return;
 
   const { tabsInfo } = await chrome.storage.sync.get("tabsInfo");
-
-  console.log("on alarm get tabsInfo", tabsInfo);
 
   chrome.tabs.query({}, function (tabs) {
     tabs.forEach((tab) => {
@@ -56,8 +50,6 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
       }
     });
   });
-
-  console.log("onAlarm setting tabsInfo", tabsInfo);
 
   // save tabsInfo back to storage
   chrome.storage.sync.set({ tabsInfo });
