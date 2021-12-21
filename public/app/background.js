f94b42e0138ef77ad31f59701dfc3434a5fdcb14
Jared Goldstein <jared.goldstein@ladbrokes.com.au>
2021-12-21 11:04:39 +1100

    add cpu info function

---
+++ public/app/background.js
@@ -0,0 +1,9 @@
+chrome.tabs.onActivated.addListener((tab) => {
+  console.log("tab is active");
+  console.log(getCpuInfo());
+});
+
+const getCpuInfo = async () => {
+  const cpuInfo = await chrome.system.cpu.getInfo();
+  return cpuInfo
+}
